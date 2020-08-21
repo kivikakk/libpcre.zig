07e08febeff0a67cc6bee5169f5e784ab4a9074a
Asherah Connor <ashe@kivikakk.ee>
2020-08-21 13:18:53 +1000

    rename and make public linkPcre

--- build.zig
+++ build.zig
@@ -4,18 +4,18 @@ pub fn build(b: *std.build.Builder) !void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("libpcre.zig", "src/main.zig");
     lib.setBuildMode(mode);
-    try addCommonRequirements(lib);
+    try linkPcre(lib);
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
-    try addCommonRequirements(main_tests);
+    try linkPcre(main_tests);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 }
 
-fn addCommonRequirements(exe: *std.build.LibExeObjStep) !void {
+pub fn linkPcre(exe: *std.build.LibExeObjStep) !void {
     exe.linkLibC();
     if (std.builtin.os.tag == .windows) {
         try exe.addVcpkgPaths(.Static);
