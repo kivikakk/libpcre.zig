414c7f6770394d1ea40ef3a5458cd0a6c307d5f1
Meghan Denny <hello@nektro.net>
2021-08-06 19:44:08 -0700

    update build.zig to use Zigmod

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const deps = @import("./deps.zig");
 
 pub fn build(b: *std.build.Builder) !void {
     const mode = b.standardReleaseOptions();
@@ -7,24 +8,14 @@ pub fn build(b: *std.build.Builder) !void {
     const lib = b.addStaticLibrary("libpcre.zig", "src/main.zig");
     lib.setBuildMode(mode);
     lib.setTarget(target);
-    try linkPcre(lib);
+    deps.addAllTo(lib);
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
-    try linkPcre(main_tests);
     main_tests.setBuildMode(mode);
     main_tests.setTarget(target);
+    deps.addAllTo(main_tests);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
-}
-
-pub fn linkPcre(exe: *std.build.LibExeObjStep) !void {
-    exe.linkLibC();
-    if (std.builtin.os.tag == .windows) {
-        try exe.addVcpkgPaths(.static);
-        exe.linkSystemLibrary("pcre");
-    } else {
-        exe.linkSystemLibrary("libpcre");
-    }
 }
